945114211034bc3e3a30fece155e4fa33c6ccc0a
Matthew Ogtong <ogtongm@gmail.com>
2020-10-22 18:03:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -41,7 +41,38 @@ def game_hash()
     },
     :away =>
     {
-
+      :team_name => "",
+      :colors => ["",""],
+      :players =>
+      [
+        {
+          :player_name =>
+        },
+        {
+          :number =>
+        },
+        {
+          :shoe =>
+        },
+        {
+          :points =>
+        },
+        {
+          :rebounds =>
+        },
+        {
+          :assists =>
+        },
+        {
+          :steals =>
+        },
+        {
+          :blocks =>
+        },
+        {
+          :slam_dunks =>
+        }
+      ]
     }
   }
 
